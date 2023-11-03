41b13d687dde6bb79eaa35558ba543a16c1290f3
Heera Rana <heerarana79352@gmail.com>
2023-11-03 16:03:48 +0530

    New c++ program

---
+++ p1.cpp
@@ -0,0 +1,17 @@
+//To calculate the sum of two numbers.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int num1,num2;
+cout<<"\nEnter the first Number: ";  //"<<" this is Insertion operator
+cin>>num1;  //">>" this is Extraction operator
+
+cout<<"\nEnter the second Number: ";
+cin>>num2;
+
+cout<<"\nThe sum is: "<<num1+num2;
+return 0;
+}
